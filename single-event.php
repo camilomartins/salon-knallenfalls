369d7a48bc1c1d9467499b2f4cd07128ef4d5b7a
camilomartins <66945701+camilomartins@users.noreply.github.com>
2023-02-27 23:54:53 +0000

    Styling Fixes

--- single-event.php
+++ single-event.php
@@ -9,35 +9,36 @@
 <?php get_header(); ?>
 
 <div class="" id="post-<?php the_ID(); ?>" > <?php// post_class(); ?>
-	<div class="relative    md:px-12 z-10 container shrink mx-auto  min-h-screen">
-		<div id="post-<?php the_ID(); ?>" <?php post_class('mb-24 flex flex-wrap'); ?>> 
-				<div class=" md:w-1/2">
-                  
-                    <div class=" md:w-full ">
-						<div class=" relative mb-20">
-							<div>
-							<?php  
-							$image = get_field('event-image');
-							$size = 'square_l'; // (thumbnail, medium, large, full or custom size)
-							if( $image ) {
-							echo wp_get_attachment_image( $image, $size );
-							}
-						?>
+	<div class="relative    md:px-12 container shrink mx-auto  min-h-screen">
+		<div id="post-<?php the_ID(); ?>" <?php post_class('mb-24 flex flex-wrap md:flex-nowrap'); ?>> 
+				<div class=" md:w-1/2 ">                                      
+						<div class="relative">
+							<div class="z-0 bg-slate-300">
+								<?php  
+								$image = get_field('event-image');
+								$size = 'square_l'; // (thumbnail, medium, large, full or custom size)
+								if( $image ) {
+								echo wp_get_attachment_image( $image, $size );
+								}
+								?>
 							</div>
-							<div class="hover:animate-spin-slow font-bold font-serif text-xl w-40 h-40 text-black bg-white flex place-items-center rounded-full relative md:absolute mb-20 md:-bottom-20  ml-4 md:-right-20">
-								<div class=" aligncenter text-center ">
+						
+							<div class="z-50 -mt-16 md:-mr-0 -mr-3 object-center float-right hover:animate-spin-slow font-bold font-serif text-xl w-40 h-40 text-black bg-white  place-items-center relative rounded-full md:absolute md:-bottom-20  md:-right-20">
+								<div class="w-40 h-40 text-center flex place-items-center">
+									<div>
 									<?php 
 										$unixtimestamp = strtotime( get_field('event-date') );
 										// Display date in the format "l d F, Y".
 										the_field("event-date");
-									?>															
+									?>	
+									</div>														
 								</div>
 							</div>
-						</div>	
-                    </div>				                	
+						</div>
+                    			                	
 				</div>
 				  
-				<div class="md:ml-24 mt-24 md:mt-60 md:p-6 md:w-1/2 md:pl-16 md:pt-6 text-primary place-items-center">
+				<div class="ml-0 md:ml-24 mt-10 md:mt-60 md:p-6 md:w-1/2 md:pl-16 md:pt-6 text-primary place-items-center">
                   	<div class="">
 				 		<h2 class="font-serif bold text-primary entry-title text-xl md:text-2xl font-extrabold leading-snug  mb-4">
                     		<?php the_title(); ?>
